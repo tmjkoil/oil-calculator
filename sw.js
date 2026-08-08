24eefb483e7362ff0d37cf333d7e2590e5cdcca3
tmjkoil <y.themachine@gmail.com>
2026-08-08 16:29:10 +0800

    Publish calculator static site

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "oil-calculator-v1";
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["./", "./index.html", "./manifest.webmanifest", "./app-icon.png"]))));
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => {
+    if (cached) return cached;
+    return fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./"));
+  }));
+});
