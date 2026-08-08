63e3b694714e22bca2389c8b50e995bafe4fd2a9
tmjkoil <y.themachine@gmail.com>
2026-08-08 20:46:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "oil-calculator-v1";
+const CACHE = "oil-calculator-v2";
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["./", "./index.html", "./manifest.webmanifest", "./app-icon.png"]))));
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then((cached) => {
